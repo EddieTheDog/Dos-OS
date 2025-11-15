59ddc9d2220ca5b626315494fd4fab59a222799e
EddieTheDog <1dylanroeder1@gmail.com>
2025-11-15 12:46:11 -0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,15 +2,16 @@ const CACHE_NAME = 'dosos26-cache-v1';
 const urlsToCache = [
   '/',
   '/index.html',
+  '/user.html',
   '/styles.css',
   '/main.js',
-  '/manifest.json',
+  '/userMain.js',
+  '/manifest.json'
 ];
 
 self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
